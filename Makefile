1d9dea2b1f095f3dc4dcaf749378fa5206273b49
Анатолий Пичугин <seurlight@MacBook-Air-Anatolij.local>
2024-05-01 14:08:37 +0300

    added gendiff.js gitignored node modules and makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 install:
 	npm ci
+publish:
+	npm publish --dry-run
+gendiff:
+	node gendiff.js
---
+++ gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+program.parse();
